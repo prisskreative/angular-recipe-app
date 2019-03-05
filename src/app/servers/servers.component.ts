a984c3f6909d81e62e753c4302444e1257f21b5d
Priscilla <priss@prisskreative.com>
2019-03-05 16:26:38 -0500

    Angular directives

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -16,6 +16,9 @@ export class ServersComponent implements OnInit {
   serverName = 'Testserver';
   username = '';
   serverCreated = false;
+  servers = ['Testserver', 'Testserver 2'];
+  showSecret = false;
+  log = [];
 
   constructor() { 
     setTimeout(() => {
@@ -30,12 +33,19 @@ export class ServersComponent implements OnInit {
   // method - event binding click
   onCreateServer() {
     this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
 
   onUpdateServerName(event: any){
     //console.log(event);
     this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+  onToggleDetails() {
+    this.showSecret = !this.showSecret;
+    //this.log.push(this.log.length + 1);
+    this.log.push(new Date());
   }
 
 }
